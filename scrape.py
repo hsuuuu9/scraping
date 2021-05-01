105a73c07717c647d264bd35d0bb7b8b9c29a12f
shuichi <75147159+hsuuuu9@users.noreply.github.com>
2021-05-01 11:54:21 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -9,6 +9,7 @@ import datetime
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 #options.add_argument("--headless")
+options.add_argument("--no-sandbox")
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument('--user-data-dir=/home/ubuntu/.config/google-chrome')#user
 options.add_argument('--profile-directory=Default')
@@ -26,6 +27,30 @@ renderer="Intel Iris OpenGL Engine",
 fix_hairline=True,
 )
 
-url = "https://item.rakuten.co.jp/project1-6/4530956592015/"
+url = "https://item.rakuten.co.jp/project1-6/4530956591629/"
 
 driver.get(url)
+t = False
+while t == False:
+    if datetime.datetime.now() > datetime.datetime(2021, 5, 1, 11, 59, 59):
+        t = True
+driver.refresh()
+for i in range(40000):
+    try:
+        driver.find_element_by_css_selector(".cart-button.checkout.new-cart-button").click()
+        break
+    except:
+        driver.refresh()
+for i in range(4000):
+    try:
+        driver.find_element_by_css_selector(".big-red-button.large-button.purchaseButton.ratTrackingEvent").click()
+        break
+    except:
+        pass
+for i in range(4000):
+    try:
+        driver.find_element_by_name("commit").click()
+        break
+    except:
+        pass
+driver.quit()
